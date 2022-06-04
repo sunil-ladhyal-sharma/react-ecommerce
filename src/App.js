51ff1d6c31e1903cad0304e59febc22ebfaf8b52
Sunil Sharma <sunil.ladhyal.sharma@gmail.com>
2022-06-04 19:19:09 +0530

    feat(react-ecomerce-4): Navigatore bar fixed. During scroll up & down navigation bar is available.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.scss';
+import logo from "./logo.svg";
+import "./App.scss";
 // import './feature/CSS/feature.css';
 // import { Counter } from './features/counter/Counter';
-import Home from './features/Home.js/Home';
+import Home from "./features/Home.js/Home";
+import Navigation from "./features/Navigation/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <div className="pb-5">
+        <Navigation />
+      </div>
       <Home />
     </div>
   );
--- src/features/Home.js/Home.js
+++ src/features/Home.js/Home.js
@@ -1,54 +1,46 @@
 import React, { useEffect, useState } from "react";
 import api from "../../server-api/api";
-import Footer from "../Footer/Footer";
-import Navigation from "../Navigation/Navigation";
+// import Footer from "../Footer/Footer";
+// import Navigation from "../Navigation/Navigation";
 import Products from "../Shared/Products";
 
 function Home() {
-    const [allProduct, setProduct] = useState([]);
-    const product = async () => {
-         const response = await api.get("products");
-         return response.data;
-    }
+  const [allProduct, setProduct] = useState([]);
+  const product = async () => {
+    const response = await api.get("products");
+    return response.data;
+  };
 
-    useEffect(() =>{
-     
-       product().then( (data) => {
-            console.log(data);
-            setProduct(data);
-            console.log(allProduct);
-        })
-        // console.log(getAllProduct());
-    }, []);
+  useEffect(() => {
+    product().then((data) => {
+      console.log(data);
+      setProduct(data);
+      console.log(allProduct);
+    });
+    // console.log(getAllProduct());
+  }, []);
 
   return (
     <>
-
-      <div>
-        <Navigation />
-      </div>
-      <div className="container-fluid">
-      
-         <div className="row justify-content-center">
+      <div className="container-fluid pt-3">
+        <div className="row justify-content-center">
           {allProduct.map((itm) => (
             //    <li id={itm.id}>{itm.id}</li>
-              <Products key={itm.id} items={itm} className=" d-flex py-5" />
+            <Products key={itm.id} items={itm} className=" d-flex py-5" />
             //   console.log(itm);
           ))}
           {/* ///{allProduct} */}
-        {/* {
+          {/* {
             allProduct.map((item) => (
                 <Products items={item}/>
             ))
         }     */}
-{/* <Products items={allProduct} /> */}
-      
+          {/* <Products items={allProduct} /> */}
+        </div>
       </div>
-  
-      </div>
-      <div className="Footer">
+      {/* <div className="Footer">
         <Footer />
-      </div>
+      </div> */}
     </>
   );
 }
